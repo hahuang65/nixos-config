69cc6f488f39e413210c610facbf73c114e1eab8
Howard Huang <hao@hwrd.me>
2024-09-19 23:46:52 -0500

    Switch senpai configuration off home-manager module
    Since it's not as flexible and doesn't provide many of senpai's options

--- home/senpai/default.nix
+++ home/senpai/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   inherit (lib) mkEnableOption mkIf;
@@ -13,20 +18,26 @@ in
   config = mkIf config.senpai.enable {
     sops.secrets."sourcehut/chat/token" = { };
 
-    programs.senpai = {
-      enable = true;
-      config = {
-        address = "chat.sr.ht";
-        nickname = "hwrd";
-        password-cmd = [
-          "cat"
-          config.sops.secrets."sourcehut/chat/token".path
-        ];
-      };
-    };
+    home = {
+      packages = [ pkgs.senpai ];
+      file = {
+        "${config.xdg.configHome}/senpai/highlight".source = ./highlight;
 
-    home.file = {
-      "${config.xdg.configHome}/senpai/highlight".source = ./highlight;
+        # FIXME: Change these params into options that can be passed in
+        "${config.xdg.configHome}/senpai/senpai.scfg".text = ''
+          address "chat.sr.ht"
+          nickname "hwrd"
+          realname "Howard Huang"
+          password-cmd cat ${config.sops.secrets."sourcehut/chat/token".path}
+          highlight hwrd hahuang65
+
+          pane-widths {
+            channels 20
+            nicknames 10
+            members 14
+          }
+        '';
+      };
     };
   };
 }
