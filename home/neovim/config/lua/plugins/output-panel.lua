3645d36ddd966ac182639789805ab80738048369
Howard Huang <hao@hwrd.me>
2025-01-16 10:42:41 -0600

    Update output-panel settings

--- home/neovim/config/lua/plugins/output-panel.lua
+++ home/neovim/config/lua/plugins/output-panel.lua
@@ -3,9 +3,19 @@
 return {
   "mhanberg/output-panel.nvim",
   name = "vimplugin-mhanberg-output-panel.nvim",
-  cmd = "OutputPanel",
   event = "LspAttach",
   config = function()
-    require("output_panel").setup()
+    require("output_panel").setup({
+      max_buffer_size = 5000, -- default
+    })
   end,
+  cmd = { "OutputPanel" },
+  keys = {
+    {
+      "<leader>o",
+      vim.cmd.OutputPanel,
+      mode = "n",
+      desc = "Toggle the output panel",
+    },
+  },
 }
