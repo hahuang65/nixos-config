25279263940ce4352bc2bfe7917ed2ed1abaf398
Howard Huang <hao@hwrd.me>
2025-02-03 14:06:26 -0600

    Switch to JetBrains Mono for font

--- hosts/common/optional/styles/default.nix
+++ hosts/common/optional/styles/default.nix
@@ -62,7 +62,7 @@ in
           monospace = mkOption {
             type = types.str;
             description = "Name of the monospace font to use";
-            default = "Departure Mono";
+            default = "JetBrains Mono";
           };
 
           emoji = mkOption {
