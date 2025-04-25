c4895569ee78f61a81aac26174894561eda7d983
Howard Huang <hao@hwrd.me>
2025-04-25 12:33:59 -0500

    Improve bluetooth config for audio

--- hosts/common/linux/optional/bluetooth.nix
+++ hosts/common/linux/optional/bluetooth.nix
@@ -21,23 +21,27 @@ in
       powerOnBoot = true;
       settings = {
         General = {
-          Enable = "Source,Sink,Media,Socket";
           Experimental = true;
         };
       };
     };
-    hardware.pulseaudio.configFile = pkgs.writeText "default.pa" ''
-      load-module module-bluetooth-policy auto_switch=2
-      load-module module-bluetooth-discover
-      ## module fails to load with 
-      ##   module-bluez5-device.c: Failed to get device path from module arguments
-      ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
-      # load-module module-bluez5-device
-      # load-module module-bluez5-discover
-    '';
-    hardware.pulseaudio.extraConfig = "
-      load-module module-switch-on-connect
-    ";
+
+    hardware.pulseaudio = {
+      enable = false; # We're using PipeWire
+      package = pkgs.pulseaudioFull;
+      configFile = pkgs.writeText "default.pa" ''
+        load-module module-bluetooth-policy auto_switch=2
+        load-module module-bluetooth-discover
+        ## module fails to load with 
+        ##   module-bluez5-device.c: Failed to get device path from module arguments
+        ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
+        # load-module module-bluez5-device
+        # load-module module-bluez5-discover
+      '';
+      extraConfig = "
+        load-module module-switch-on-connect
+      ";
+    };
 
     systemd.user.services.mpris-proxy = {
       description = "Mpris proxy";
