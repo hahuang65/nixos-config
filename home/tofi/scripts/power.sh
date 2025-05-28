d1ce09da291967e9344a31d4da472f0b2608b58c
Howard Huang <hao@hwrd.me>
2025-05-28 02:30:15 -0500

    Add screen lock to tofi scripts

--- home/tofi/scripts/power.sh
+++ home/tofi/scripts/power.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-case "$(printf "reboot\nshutdown" | tofi --num-results 2)" in
+case "$(printf "lock\nreboot\nshutdown" | tofi)" in
+lock) swaylock --daemonize --indicator --screenshots --clock --effect-greyscale --effect-pixelate 5 ;; # FIXME: Can I deduplicate this with what's in sway/default.nix?
 reboot) systemctl reboot -i ;;
 shutdown) shutdown now ;;
 *) exit 1 ;;
