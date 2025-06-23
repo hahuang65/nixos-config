5291f672cbf4a5fbe41f58ecafe0a0e065721ffb
Howard Huang <hao@hwrd.me>
2025-06-22 23:09:50 -0500

    Fix tofi-srun searching for scripts

--- home/tofi/scripts/srun.sh
+++ home/tofi/scripts/srun.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 script="tofi-$(
-  find "$HOME/.nix-profile/bin" -maxdepth 1 -executable -name 'tofi-*' -exec basename {} \; |
+  find "$HOME/.nix-profile/bin/" -maxdepth 1 -executable -name 'tofi-*' -exec basename {} \; |
     grep -v "tofi-run" |
     grep -v "tofi-drun" |
     grep -v "tofi-srun" |
